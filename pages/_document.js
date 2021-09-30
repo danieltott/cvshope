88bef0acc762f325633e36f4448162d20039d46d
Dan Ott <dan@dtott.com>
2021-09-30 15:49:41 -0400

    add tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,24 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* <!-- Facebook Pixel Code --> */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1062677141208647');
+fbq('track', 'PageView');
+`,
+            }}
+          />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
@@ -27,6 +45,13 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <body>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              src="https://www.facebook.com/tr?id=1062677141208647&ev=PageView&noscript=1"
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
